4747e1da5fb9e3ca3abf778c25e2e717c39b3bca
RohitBacher <rohitbacher956@gmail.com>
2026-06-28 21:12:06 +0530

    Add dictionary example with keys and values printing

---
+++ Python/Data_Structures/Assignment/dictionary/4.py
@@ -0,0 +1,13 @@
+dic = {1: "Apple", 2: "Banana", 3: "Mango"}
+
+print("Keys:")
+for key in dic:
+    print(key)
+
+print("Values:")
+for value in dic.values():
+    print(value)
+
+print("Keys and Values:")
+for key, value in dic.items():
+    print(key, value)
